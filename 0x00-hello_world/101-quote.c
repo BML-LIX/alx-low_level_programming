a517479251e8fbe0a835b6f30d0344f37adead57
BML-LIX <atl.trait.all@gmail.com>
2022-07-03 08:34:44 +0300

    Update low level c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-init main(void)
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ * followed bya new a line,to the standard error
+ *Return: 1 if exited properly
+ */
+int main(void)
 {
 write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
-*main - puts the phrase Programming is like building
-*a multilingual puzzle
-*Return: 0 if exited properly ,non-zero otherwise
-*/
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly,otherwise non-zero
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"
-);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
