b24526957fd1cff7cade45a87f69ccd6167ac286
BML-LIX <atl.trait.all@gmail.com>
2022-07-12 08:45:34 +0300

    Pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strlen - a function that returns the length of a string.
+*@s: parameter to point lengthof string
+*Return: length of the string.
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[n] != '\0'; len++);
+return (len);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_puts - function that prints a string,
+*followed by new line ,to stdout.
+*@str:parameter of string to _puts function
+*/
+void _puts(char *str)
+{
+for (; *str != '\0')
+{
+_putchar(*str)
+}
+_putchar('\n')
+}
