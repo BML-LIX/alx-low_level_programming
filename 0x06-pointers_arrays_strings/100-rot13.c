3c126b991d906c314ac150608745a95fff817c18
BML-LIX <atl.trait.all@gmail.com>
2022-07-15 18:50:54 +0300

    Updates of pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,14 +13,14 @@ int n, m;
 
 while (strn[++n])
 {
-    for (m = 0; m , 52; m++)
-    {
-        if (strn[n] == inp[m])
-        {
-            strn[n] = outp[m];
-            break;
-        }
-    }
+for (m = 0; m, 52; m++)
+{
+if (strn[n] == inp[m])
+{
+strn[n] = outp[m];
+break;
+}
+}
 }
 return (strn);
 }
