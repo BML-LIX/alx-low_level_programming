3c351b37b3ef839155494c17d1896e80b2508ab1
BML-LIX <atl.trait.all@gmail.com>
2022-07-28 06:12:24 +0300

    Update malloc tasks late

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,35 +1,36 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 *alloc_grid - allocating grid(2d array)
 *@width: width of array
 *@height: height of the array
-*Retrun: a pointer to allocated grid
+*Return: a pointer to allocated grid
 */
 int **alloc_grid(int width, int height)
 {
 int n, n1, m, p;
 int **a;
 
-a = [
-    [1,2,3],
-    [2,3,5],
-    [8,9,7]
-]
-
 if (width <= 0 || height <= 0)
+{
 return (NULL);
-a = malloc(height * sizeof(int *));
-if (a == NULL)
-{int **alloc_grid(int width, int height)
-{
-a[n] = malloc(width * sizeof(int));
-if (a[n] == NULL)
-{
-for (n1 = n; n1 >= 0; n1++)
-{
-free(a[n1]);
 }
+else
+{
+a = (int **)malloc(height * sizeof(int *));/*malloc per pointer*/
+if (!a)
+{
+free(a);
+return (NULL);
+}
+for (n = 0; n < height; n++)
+{
+a[n] = (int *)malloc(width * sizeof(int));
+if (!a[n])
+{
+for (n1 = 0; n1 <= n; n1++)
+free(a[n1]);
 free(a);
 return (NULL);
 }
@@ -37,7 +38,10 @@ return (NULL);
 for (m = 0; m < height; m++)
 {
 for (p = 0; p < width; p++)
+{
 a[m][p] = 0;
 }
+}
+return (a);
 }
 }
