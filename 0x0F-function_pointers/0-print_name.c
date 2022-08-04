d3ddd65863050fec15c4535d883a2bc69052f471
BML-LIX <atl.trait.all@gmail.com>
2022-08-04 10:09:50 +0300

    Function pointers hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: string
- * @f: function
- */
+*print_name - prints a name
+*@name: string of a name
+*@f: a function
+*Return: nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL)
-	f(name);
+f(name);
 }
