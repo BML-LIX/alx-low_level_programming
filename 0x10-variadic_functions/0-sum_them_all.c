81c6a9a472b237d1fa0215e382e417da27eea4ae
BML-LIX <atl.trait.all@gmail.com>
2022-08-05 00:00:17 +0300

    Sum them all and README.md

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+/**
+ *sum_them_all - sum all argument
+ *@n: number of argument to sum
+ *Return: sum of all argument
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int m = 0;
+	int sum = 0;
+
+	va_list a;
+
+	va_start(a, n);
+
+	if (!n)
+		return (0);
+	for (m = 0; m < n; m++)
+		sum += va_arg(a, int);
+	va_end(a);
+	return (sum);
+}
+
